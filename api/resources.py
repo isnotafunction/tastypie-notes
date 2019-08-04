255449315688ae9234ffeee9da0550a6ee650cda
isnotafunction <ivana.cosgrove@gmail.com>
2019-08-04 16:09:52 +0100

    set up resources

---
+++ api/resources.py
@@ -0,0 +1,11 @@
+from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
+from api.models import Note
+
+
+class NoteResource(ModelResource):
+    class Meta:
+        queryset = Note.objects.all()
+        resource_name = "note"
+        authorization = Authorization()
+        fields = ["title", "body"]
